62a3d3a232efac6ce85ef513706e21b02174dc57
Eder Gomes <edergomesj@gmail.com>
2025-03-11 15:58:43 -0300

    Feat: Add navbar-toggle collapse small devices

--- js/script.js
+++ js/script.js
@@ -1,10 +1,29 @@
 
 
-/*$(document).ready(function() {
-    $(".nav-item").click(function(){
-        $(".collapse").collapse('hide');
+//Collapses navbar-toggler when a link is clicked, sets link to start of section.
+$(document).ready(function() {
+    $(".navbar-nav a, .navbar-brand").click(function(event) {
+        if (this.hash !== "") {  // Check if has a hash (ex: #about, #portfolio).
+            event.preventDefault(); // Prevents browser behavior.
+
+            var target = $(this.hash); // Select the target element by ID, ex: #about, #portfolio, etc.
+            var navbarHeight = $(".navbar").outerHeight(); // Get height of navbar.
+
+            if (target.length) {
+                $("html, body").animate({
+                    scrollTop: target.offset().top - navbarHeight // Adjust final position.
+                }, 100); // animation duration (100ms).
+            }
+
+            // Close the navbar
+            if ($(".navbar-toggler").is(":visible")) {
+                $(".navbar-collapse").collapse('hide');
+            }
+        }
     });
-});*/
+});
+
+
 
 
 document.addEventListener("DOMContentLoaded", function () {
